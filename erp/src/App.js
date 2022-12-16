3767390b87dacdd0030f87b5091e628d0cf689af
Jacob Yang <jacob.yang@gordon.edu>
2022-12-16 00:50:27 -0500

    removed node_modules from main directory

--- erp/src/App.js
+++ erp/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
                 <div className='flex relative dark:bg-main-dark-bg'>
                     <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
                         <TooltipComponent content="Settings" position = "Top">
-                            <button>
+                            <button type="button" className='text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray'>
                                 <FiSettings/>
                             </button>
                         </TooltipComponent>
