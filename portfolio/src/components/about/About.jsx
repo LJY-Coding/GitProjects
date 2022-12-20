2d633b593c8cc1434cb8af756274176f2b6b863d
Jacob Yang <jacob.yang@gordon.edu>
2022-12-19 23:59:28 -0500

    portfolio about css commit

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.jpg'
-
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
@@ -14,14 +16,35 @@ const About = () => {
           <div className="about__me-img">
             <img src={ME} alt="About Image" />
           </div>
-        </div>
+        
 
         <div className="about__content">
           <div className="about__cards"></div>
             <article className="about__card">
+              <FaAward className='about__icon'/>
               <h5>Experience</h5>
               <small>3+ Years Working</small>
             </article>
+
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+        </div>
+
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, quidem nihil? Adipisci obcaecati quaerat eligendi odio quos, magnam iusto neque laborum excepturi sapiente, nam consequatur placeat, natus labore quae rem.
+        </p>
+
+        <a href="#contact" className='btn btn-primary'>Let's talk</a>
         </div>
       </div>
     </section>
